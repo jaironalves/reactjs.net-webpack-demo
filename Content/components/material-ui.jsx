48fe107774c7212bd69164ec3d988c14463564d0
Jairon Alves Lima <jairon.lima@linx.com.br>
2020-05-02 12:57:46 -0300

    Upgrade material-ui example

--- Content/components/material-ui.jsx
+++ Content/components/material-ui.jsx
@@ -1,25 +1,64 @@
 import React, { Fragment, useEffect } from 'react';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import { Helmet } from 'react-helmet';
 
-export function MaterialUIDemo() {
-	//useEffect(() => {
-	//	//const jssStyles = document.querySelector('#material-ui-server-side');
-	//	//if (jssStyles) {
-	//		//jssStyles.parentElement.removeChild(jssStyles);
-	//	}
-	//}, []);
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#fff',
+        },
+    },
+});
 
-	return (
-		<Fragment>
-			<Helmet>
-				<title>ReactJS.NET Demos | Material-UI</title>
-			</Helmet>
-			<Container maxWidth="sm">
-				Hello from{' '}
-				<a href="https://github.com/mui-org/material-ui">Material-UI</a>!
-			</Container>
-		</Fragment>
-	);
+const useStyles = makeStyles({
+    root: {        
+        color: '#222',             
+        textShadow: '0 0 5px lightgray',        
+    },
+    link: {
+        transition: 'color 0.2s ease',
+        color: 'palevioletred',
+        textDecoration: 'none',
+        '&:hover': {
+            color: '#888',
+        }
+    },
+});
+
+export function MaterialUIDemo() {
+    useEffect(() => {
+        const jssStyles = document.querySelector('#material-ui-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    const classes = useStyles();
+
+    return (
+        <Fragment>
+            <Helmet>
+                <title>ReactJS.NET Demos | Material-UI</title>
+            </Helmet>
+            <ThemeProvider theme={theme}>
+                <Typography className={classes.root} variant="h3">
+                    Hello from
+                    <Link className={classes.link} href="https://github.com/mui-org/material-ui">Material-UI</Link>!
+			    </Typography>
+            </ThemeProvider>
+        </Fragment>
+    );
 }
